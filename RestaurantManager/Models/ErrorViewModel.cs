a94a522683800f961ba33515616363b1fa542769
shemsAsf <137190111+shemsAsf@users.noreply.github.com>
2026-05-11 10:33:29 +0200

    Init Project - Basic Visual Studio MVC Model

---
+++ RestaurantManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
